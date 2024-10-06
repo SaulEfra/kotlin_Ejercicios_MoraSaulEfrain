1660bae87b235d4c4216b1ccdbcfc47a5b13b8d7
saul mora <kennykurosaki2000@gmail.com>
2024-10-06 17:16:07 -0600

    Ejercicios Terminados

--- src/Bucles/029-ej-multiplos-5-1.kt
+++ src/Bucles/029-ej-multiplos-5-1.kt
@@ -6,13 +6,13 @@ fun main (){
         var num = readln().toInt();
 
         for (i in 1 .. num){
-//            if (i % 5 == 0){
-//                println("$i")
-//            }
+            if (i % 5 == 0){
+                println("$i")
+            }
             // lo que vi en mi investigacion es que los multiplos de un numero
             // son el resultado de la multiplicacion del numero por otro numero
-            var res = num * i;
-            println(" $res")
+//            var res = num * i;
+//            println(" $res")
         }
     }catch (e:NumberFormatException)
     {
